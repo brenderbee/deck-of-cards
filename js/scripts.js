b96f3458dbc82c0ee43df5033cfc620196f23175
Brenda Franco and Matt Dowdney <brenda@>
2018-03-26 15:50:23 -0700

    implement more array logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+  var suits = ["Spades", "Clubs", "Hearts", "Diamonds"];
+  var ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 
+  suits.forEach(function(suit) {
+    var card = suits.pop();
+    ranks.forEach(function(rank) {
+      $("#output").append("<li>" + rank + " of " + card + "</li>");
+    });
+  });
 
 });
